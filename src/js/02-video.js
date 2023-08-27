ef393798b3e1ceab5e21f2a432dab766e8fbc69f
Fuasma2727 <jmamgb127@gmail.com>
2023-08-27 10:51:57 -0500

    last commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+// Obtén una referencia al elemento de video utilizando player.js
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const timeThrottled = _.throttle(function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+  }, 1000);
+  
+  player.on('timeupdate', function(data) {
+    timeThrottled(data); 
+  });
+
+let seconds = localStorage.getItem("videoplayer-current-time");
+
+
+player.setCurrentTime(seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+let submit = document.querySelector('button[type="submit"]');
+let input1 = document.querySelector('input');
+let input2 = document.querySelector('textarea');
+
+const submitHandler = () => {
+    const inputValue = input1.value;
+    const textareaValue = input2.value;
+    
+    let feedbackArray = [inputValue, textareaValue];
+    let feedbackArrayJSON = JSON.stringify(feedbackArray);
+    localStorage.setItem("feedback-form-state", feedbackArrayJSON);
+
+    console.log(inputValue);
+    console.log(textareaValue);
+};
+
+// Utiliza lodash.throttle para limitar la ejecución de submitHandler a una vez cada 500 ms
+const throttledSubmitHandler = throttle(submitHandler, 500);
+submit.addEventListener('click', throttledSubmitHandler);
+
+
+window.addEventListener('load', () => {
+    let storedFeedbackArrayJSON = localStorage.getItem("feedback-form-state");
+    let storedFeedbackArray = JSON.parse(storedFeedbackArrayJSON);
+    
+    if (storedFeedbackArray) {
+        input1.value = storedFeedbackArray[0];
+        input2.value = storedFeedbackArray[1];
+    }
+});
